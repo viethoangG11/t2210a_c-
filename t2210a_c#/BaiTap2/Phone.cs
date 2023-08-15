1f3acd7bb83e17fcbd5c6500ccf7ab9eceeb1d1e
viethoangG11 <hoangyla11112004@gmail.com>
2023-08-15 16:41:00 +0700

    BTSlot5

---
+++ t2210a_c#/BaiTap2/Phone.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+abstract class Phone
+{
+    public abstract void InsertPhone(string name, string phone);
+    public abstract void RemovePhone(string name);
+    public abstract void UpdatePhone(string name, string newphone);
+    public abstract void SearchPhone(string name);
+    public abstract void Sort();
+}
+class PhoneBook : Phone
+{
+    private List<(string name, string phone)> PhoneList;
+
+    public PhoneBook()
+    {
+        PhoneList = new List<(string name, string phone)>();
+    }
+    public override void InsertPhone(string name, string phone)
+    {
+        var index = PhoneList.FindIndex(x => x.name == name);
+        if (index == -1)
+        {
+            PhoneList.Add((name, phone));
+            Console.WriteLine("Thêm thành công!");
+        }
+        else
+        {
+            var phones = PhoneList[index].phone.Split(",");
+            if (!phones.Contains(phone))
+            {
+                PhoneList[index] = (name, PhoneList[index].phone + "," + phone);
+                Console.WriteLine("Thêm thành công!");
+            }
+            else
+            {
+                Console.WriteLine("Số điện thoại đã tồn tại!");
+            }
+        }
+    }
+    public override void RemovePhone(string name)
+    {
+        var index = PhoneList.FindIndex(x => x.name == name);
+        if (index == -1)
+        {
+            Console.WriteLine("Không tìm thấy người dùng!");
+        }
+        else
+        {
+            PhoneList.RemoveAt(index);
+            Console.WriteLine("Xóa thành công!");
+        }
+    }
+    public override void UpdatePhone(string name, string newphone)
+    {
+        var index = PhoneList.FindIndex(x => x.name == name);
+        if (index == -1)
+        {
+            Console.WriteLine("Không tìm thấy người dùng!");
+        }
+        else
+        {
+            PhoneList[index] = (name, newphone);
+            Console.WriteLine("Cập nhật thành công!");
+        }
+    }
+    public override void SearchPhone(string name)
+    {
+        var index = PhoneList.FindIndex(x => x.name == name);
+        if (index == -1)
+        {
+            Console.WriteLine("Không tìm thấy người dùng!");
+        }
+        else
+        {
+            Console.WriteLine("Tên: {0}", PhoneList[index].name);
+            Console.WriteLine("Số điện thoại: {0}", PhoneList[index].phone);
+        }
+    }
+    public override void Sort()
+    {
+        PhoneList.Sort((x, y) => x.name.CompareTo(y.name));
+        Console.WriteLine("Sắp xếp thành công!");
+    }
+}
---
+++ t2210a_c#/BaiTap2/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2210A_C_.BaiTap2
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Tạo một đối tượng PhoneBook
+            PhoneBook phoneBook = new PhoneBook();
+
+            // Thêm một số người dùng và số điện thoại vào PhoneBook
+            phoneBook.InsertPhone("An", "0987654321");
+            phoneBook.InsertPhone("Binh", "0123456789");
+            phoneBook.InsertPhone("An", "0912345678");
+            phoneBook.InsertPhone("Cuong", "0999999999");
+
+            // Tìm kiếm số điện thoại của An
+            phoneBook.SearchPhone("An");
+
+            // Xóa người dùng Binh khỏi PhoneBook
+            phoneBook.RemovePhone("Binh");
+
+            // Cập nhật số điện thoại của Cuong thành 0888888888
+            phoneBook.UpdatePhone("Cuong", "0888888888");
+
+            // Sắp xếp các người dùng trong PhoneBook theo tên
+            phoneBook.Sort();
+        }
+    }
+}
