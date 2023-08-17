45fb044ba11d27a96e84149baa636c22cf7d4555
viethoangG11 <hoangyla11112004@gmail.com>
2023-08-17 15:19:31 +0700

    slot11

---
+++ t2210a_c#/bank/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2210a_c_.bank
+{
+    internal class BankAccount
+    {
+        
+        public decimal Balance { get; private set; }
+
+        
+        public BankAccount(decimal initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            
+            Console.WriteLine("Bạn đã gửi {0} vào tài khoản. Số dư hiện tại là {1}.", amount, Balance);
+        }
+
+        
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                
+                Console.WriteLine("Số tiền rút vượt quá số dư. Không thể thực hiện giao dịch.");
+            }
+            else
+            {
+                Balance -= amount;
+                
+                Console.WriteLine("Bạn đã rút {0} từ tài khoản. Số dư hiện tại là {1}.", amount, Balance);
+            }
+        }
+    }
+
+}
